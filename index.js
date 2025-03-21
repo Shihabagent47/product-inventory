0d87bea8e34a9eae35d48bbbae0833668f63f023
Shihab Hossain <64682561+Shihabagent47@users.noreply.github.com>
2025-03-21 17:40:20 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const app=express();
+const path=require('path');
+const mongooes=require('mongoose');
+
+
+app.listen(3000,()=>{
+    console.log(`listening to port 3000`);
+});
