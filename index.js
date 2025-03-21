21b1b13ecbc2a5f69bedf8b6b6e98c3f41f0d580
Shihab Hossain <64682561+Shihabagent47@users.noreply.github.com>
2025-03-21 22:37:27 +0600

    feat: implemented general middleware

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express =require('express');
 const app=express();
 const path=require('path');
 const connectDB=require('./config/db')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const logger = require('morgan');
+const cors = require('cors');
+const corsOptions=require('./middleware/cors_option');
+const port = process.env.PORT;
 
+
+app.use(cors(corsOptions));
+app.use(logger('dev'));
+app.use(helmet());
+app.get('/',(req,res)=>{
+    res.send("Welcome Dude");
+});
 connectDB();
-app.listen(3000,()=>{
-    console.log(`listening to port 3000`);
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`);
 });
